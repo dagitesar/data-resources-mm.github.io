b6ad456259f726a9725905b6c84d411eada9a371
dagitesar <dagitesar@gmail.com>
2026-09-22 20:35:30 +0000

    fix: error 401

--- config.js
+++ config.js
@@ -7,5 +7,5 @@
  */
 window.SITE = {
   repo: "data-resources-mm/data-resources-mm.github.io",
-  suggestEndpoint: "https://script.google.com/macros/s/AKfycbz8APKRzydnLmfBKRc2CLLGGjfUAJ9TBFzYyLHAd0foG020JzWg7ZOVRXzLWcp7NIibaw/exec",
+  suggestEndpoint: "https://script.google.com/macros/s/AKfycbx7h02-VgyV4YEWtzpFpH6dbNitkRTV1BhYotlF1zqqlXIS2DGtc-u3aKkNpnkDR7y4Pw/exec",
 };
